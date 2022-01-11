2633316334a36aa392ee2da20b84bfca88b0196a
Vladislav Gorenkin <gorenkin@ruvents.com>
2022-01-11 12:33:46 +0600

    Explain JWT-related errors better

--- src/JwtMiddleware.php
+++ src/JwtMiddleware.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 namespace Ruvents\SpiralJwt;
 
-use Firebase\JWT\ExpiredException;
+use Firebase\JWT;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Spiral\Http\Exception\ClientException\UnauthorizedException;
 use Spiral\Http\Exception\HttpException;
 
 final class JwtMiddleware implements MiddlewareInterface
@@ -19,8 +20,12 @@ final class JwtMiddleware implements MiddlewareInterface
     ): ResponseInterface {
         try {
             return $handler->handle($request);
-        } catch (ExpiredException $exception) {
-            throw new HttpException($exception->getMessage(), 401);
+        } catch (JWT\ExpiredException $exception) {
+            throw new UnauthorizedException('JWT token is expired');
+        } catch (JWT\BeforeValidException $exception) {
+            throw new UnauthorizedException('JWT token is not valid yet');
+        } catch (JWT\SignatureInvalidException $exception) {
+            throw new UnauthorizedException('JWT token signature is invalide');
         }
     }
 }
